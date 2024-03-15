90b56b23cebedc7c6ea912d89b0c4786b11b0885
boyuan <liuboyuanl@outlook.com>
2024-03-15 10:51:07 +0800

    Site updated: 2024-03-15 10:51:07

--- js/events.js
+++ js/events.js
@@ -75,46 +75,96 @@ Fluid.events = {
   },
 
   registerScrollTopArrowEvent: function() {
-    var topArrow = jQuery('#scroll-top-button');
-    if (topArrow.length === 0) {
-      return;
+      var topArrow = jQuery('#scroll-top-button');
+      if (topArrow.length === 0) {
+        return;
+      }
+      var board = jQuery('#board');
+      if (board.length === 0) {
+        return;
+      }
+      var arrowUpIcon = jQuery('#scroll-top-button i');
+      if(arrowUpIcon.length === 0){
+        return;
+      } 
+      var posDisplay = false;
+      var scrollDisplay = false;
+      // Position
+      var setTopArrowPos = function() {
+        var boardRight = board[0].getClientRects()[0].right;
+        var bodyWidth = document.body.offsetWidth;
+        var right = bodyWidth - boardRight;
+        posDisplay = right >= 50;
+        topArrow.css({
+          'bottom': scrollDisplay ? '20px' : '-60px',
+          'right' : posDisplay ? right - 64 : 8 + 'px',
+          'min-width' : posDisplay ? 40 : 28 + 'px',
+          'min-height' : posDisplay ? 40 : 28 + 'px'
+        });
+        arrowUpIcon.css({
+          'font-size' : posDisplay ? 32 : 20 + 'px'
+        });
+      };
+      setTopArrowPos();
+      jQuery(window).resize(setTopArrowPos);
+      // Display
+      var headerHeight = board.offset().top;
+      Fluid.utils.listenScroll(function() {
+        var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
+        scrollDisplay = scrollHeight >= headerHeight;
+        topArrow.css({
+          'bottom': scrollDisplay ? '20px' : '-60px'
+        });
+      });
+      // Click
+      topArrow.on('click', function() {
+        jQuery('body,html').animate({
+          scrollTop: 0,
+          easing   : 'swing'
+        });
+      });
     }
-    var board = jQuery('#board');
-    if (board.length === 0) {
-      return;
-    }
-    var posDisplay = false;
-    var scrollDisplay = false;
-    // Position
-    var setTopArrowPos = function() {
-      var boardRight = board[0].getClientRects()[0].right;
-      var bodyWidth = document.body.offsetWidth;
-      var right = bodyWidth - boardRight;
-      posDisplay = right >= 50;
-      topArrow.css({
-        'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
-        'right' : right - 64 + 'px'
-      });
-    };
-    setTopArrowPos();
-    jQuery(window).resize(setTopArrowPos);
-    // Display
-    var headerHeight = board.offset().top;
-    Fluid.utils.listenScroll(function() {
-      var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
-      scrollDisplay = scrollHeight >= headerHeight;
-      topArrow.css({
-        'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
-      });
-    });
-    // Click
-    topArrow.on('click', function() {
-      jQuery('body,html').animate({
-        scrollTop: 0,
-        easing   : 'swing'
-      });
-    });
-  },
+  // registerScrollTopArrowEvent: function() {
+  //   var topArrow = jQuery('#scroll-top-button');
+  //   if (topArrow.length === 0) {
+  //     return;
+  //   }
+  //   var board = jQuery('#board');
+  //   if (board.length === 0) {
+  //     return;
+  //   }
+  //   var posDisplay = false;
+  //   var scrollDisplay = false;
+  //   // Position
+  //   var setTopArrowPos = function() {
+  //     var boardRight = board[0].getClientRects()[0].right;
+  //     var bodyWidth = document.body.offsetWidth;
+  //     var right = bodyWidth - boardRight;
+  //     posDisplay = right >= 50;
+  //     topArrow.css({
+  //       'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
+  //       'right' : right - 64 + 'px'
+  //     });
+  //   };
+  //   setTopArrowPos();
+  //   jQuery(window).resize(setTopArrowPos);
+  //   // Display
+  //   var headerHeight = board.offset().top;
+  //   Fluid.utils.listenScroll(function() {
+  //     var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
+  //     scrollDisplay = scrollHeight >= headerHeight;
+  //     topArrow.css({
+  //       'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
+  //     });
+  //   });
+  //   // Click
+  //   topArrow.on('click', function() {
+  //     jQuery('body,html').animate({
+  //       scrollTop: 0,
+  //       easing   : 'swing'
+  //     });
+  //   });
+  // },
 
   registerImageLoadedEvent: function() {
     if (!('NProgress' in window)) { return; }
